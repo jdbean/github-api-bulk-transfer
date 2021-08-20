1a93e8116c0d22ef8f34c10534d2a8c7332bd08b
shoya shiraki <12c1055@gmail.com>
2021-08-20 21:42:59 +0900

    fix accept header

--- bulk_transfer_repos.sh
+++ bulk_transfer_repos.sh
@@ -17,16 +17,16 @@ along with this program.  If not, see <https://www.gnu.org/licenses/
 Copyright 2019 J.D. Bean
 '
 
-function git_repo_transfer(){ 
+function git_repo_transfer(){
   curl -vL \
   	-u "$2:${GITHUB_SECRET}" \
     -H "Content-Type: application/json" \
-    -H "Accept: application/vnd.github.nightshade-preview+json" \
+    -H "Accept: application/vnd.github.v3+json" \
     -X POST https://api.github.com/repos/$1/transfer \
     -d '{"new_owner":"'$3'"}' \
     | jq .
 }
 
-repos=$( cat ./repos.txt) 
+repos=$( cat ./repos.txt)
 for repo in $repos; do (git_repo_transfer "$repo" "$1" "$2"); done
 
