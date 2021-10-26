838d0f66aa6c7e9e734076f75a7b6c50822ad570
Mr. D <danieliozzi@gmail.com>
2021-10-26 11:29:30 +0200

    update api endpoint

--- bulk_transfer_repos.sh
+++ bulk_transfer_repos.sh
@@ -21,8 +21,8 @@ function git_repo_transfer(){
   curl -vL \
   	-u "$2:${GITHUB_SECRET}" \
     -H "Content-Type: application/json" \
-    -H "Accept: application/vnd.github.nightshade-preview+json" \
-    -X POST https://api.github.com/repos/$1/transfer \
+    -H "Accept: application/vnd.github.v3+json" \
+    -X POST https://api.github.com/repos/$2/$1/transfer \
     -d '{"new_owner":"'$3'"}' \
     | jq .
 }
